64beababe25ed88fb3564a9cdc49cc2cea2e16fa
Kinan Bahsas <kinan@bahsas.me>
2022-03-27 23:51:18 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import re
+
+#delimetor between not blocks
+delim = "-----------------------------------"
+
+#regex for a note block
+block_re = re.compile(r'\n'.join([
+    r'(?P<title>.+)',
+    r'((?P<type>Lesezeichen|Markierung|Notiz)\sauf\sSeite\s(?P<page>\d+):\s)' + r'''((?P<note>(?:.|\n)*))?''' + r'"(?P<quote>(?:.|\n)*)"',
+    r'(Hinzugefügt|Geändert)\sam\s(?P<day>\d{2}).(?P<month>\d{2}).(?P<year>\d{4})',
+    ]))
+
+def main():
+    with open("notes.txt") as f:
+        text = f.read()
+        #text = text.replace(u'\xa0', u' ')
+        #text = re.sub('^\s*',"", text)
+    blocks = text.split(delim)
+    for block in blocks:
+        m = block_re.search(block)
+        if m:
+            print(m.group("title"))
+            print(m.group("quote"))
+            if m.group("note"):
+                print(m.group("note"))
+            print(delim)
+
+if(__name__=="__main__"):
+    main()
