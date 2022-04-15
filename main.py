ab2b5d5f7085c91462860120c2b5ffe6b4f1c3e7
Kinan Bahsas <kinan@bahsas.me>
2022-04-16 00:07:07 +0200

    feat: save block date and time in a datetime object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 #delimetor between not blocks
 delim = "-----------------------------------"
@@ -7,7 +8,7 @@ delim = "-----------------------------------"
 block_re = re.compile(r'\n'.join([
     r'(?P<title>.+)',
     r'((?P<type>Lesezeichen|Markierung|Notiz)\sauf\sSeite\s(?P<page>\d+):\s)' + r'''((?P<note>(?:.|\n)*))?''' + r'"(?P<quote>(?:.|\n)*)"',
-    r'(Hinzugefügt|Geändert)\sam\s(?P<day>\d{2}).(?P<month>\d{2}).(?P<year>\d{4})',
+    r'(Hinzugefügt|Geändert)\sam\s(?P<day>\d{2}).(?P<month>\d{2}).(?P<year>\d{4}) \| (?P<hour>\d{1,2}):(?P<minute>\d{2})',
     ]))
 parsed_text = ""
 
@@ -18,10 +19,11 @@ def md_export(d):
         for page in d[book]:
             parsed_text+= "- p{}\n".format(page)
             for note in d[book][page]:
-                #[quote, note] if note else [page, quote]
-                parsed_text+= "\t- > {}\n".format(note[0])
-                if len(note) > 1: # then there's a note
-                    parsed_text+= "\t\t- [[highlight/note]] {}\n".format(note[1])
+                #[date, quote, note] if note else [date, quote]
+                parsed_text+= "\t- > {}\n".format(note[1])
+                parsed_text+= "\t\t- On {}\n".format(note[0].strftime("%Y.%m.%d at %H:%M"))
+                if len(note) > 2: # then there's a note
+                    parsed_text+= "\t\t\t- [[highlight/note]] {}\n".format(note[2])
         parsed_text += delim +"\n"
     return parsed_text
 
@@ -37,7 +39,7 @@ def sanitize(unit):
 def main():
     d = {}
     # Parsing notes.txt
-    with open("real_notes.txt") as f:
+    with open("notes.txt") as f:
         text = f.read()
         #text = text.replace(u'\xa0', u' ')
         #text = re.sub('^\s*',"", text)
@@ -49,13 +51,20 @@ def main():
             page = m.group("page")
             quote = sanitize(m.group("quote"))
             note = m.group("note")
+            date_string = "{}.{}.{} at {}:{}".format(
+                                                    m.group("year"),
+                                                    m.group("month"),
+                                                    m.group("day"),
+                                                    m.group("hour"),
+                                                    m.group("minute"))
+            date = datetime.strptime(date_string, "%Y.%m.%d at %H:%M")
             if book in d.keys():
                 if page in d[book]:
-                    d[book][page].append([quote,note] if note else [quote])
+                    d[book][page].append([date, quote, note] if note else [date, quote])
                 else:
-                    d[book][page] = [[quote, note]] if note else [[quote]]
+                    d[book][page] = [[date, quote, note]] if note else [[date, quote]]
             else:
-                d[book] = {page: [[quote, note]]} if note else {page: [[quote]]} #[[page, quote, note] if note else [page, quote]]
+                d[book] = {page: [[date, quote, note]]} if note else {page: [[date, quote]]} #[[page, quote, note] if note else [page, quote]]
     print(md_export(d))
     #write_to_file(md_export(d))
 
